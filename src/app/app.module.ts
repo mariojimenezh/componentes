13d69c01292a714cbd783e1ceaa908f1bbedbce5
Mario <ceveras@gmail.com>
2018-03-05 23:03:28 +0100

    Creado componente cabecera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent
   ],
   imports: [
     BrowserModule
