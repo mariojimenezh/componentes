35e5128771f41663b54c32b03813fa79567aa12d
Mario <ceveras@gmail.com>
2018-03-06 18:47:42 +0100

    Lista formulario

--- src/app/cuerpo/cuerpo.component.ts
+++ src/app/cuerpo/cuerpo.component.ts
@@ -9,7 +9,22 @@ export class CuerpoComponent implements OnInit {
 
   mostrar: boolean = true;
   secret: string = ""
-  elemento : string [] =["Mario", "Carlos", "Antonio"]
+  elemento : string [] =["Mario", "Carlos"]
+  cambiar(event: any){
+    this.secret = event.target.value
+  }
+  agregar(valor: string){
+    //Añado a la lista si el input no está vacío
+    if(valor.length>0)
+      this.elemento.push(valor);
+  }
+  borrar(event: any){
+    
+  }
+  eliminar(){ //Elimino de la lista si no está vacía
+    if(this.elemento.length>0)
+      this.elemento.pop()
+  }
   constructor() { }
 
   ngOnInit() {
